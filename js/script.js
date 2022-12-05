82b678828564f90988eb4ed800237a5eba1b7e7d
antonionovelli92 <anto.novelli@hotmail.it>
2022-12-05 15:07:23 +0100

    add 1,2 step

--- js/script.js
+++ js/script.js
@@ -9,13 +9,22 @@ console.log('JS OK')
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
 
-/* 
-1. Chiedere quanti km si vogliono percorrere
-2. Chiedere l'eta del passeggero
-3. Calcolare il prezzo totale del viaggio (che si baserà sui km dei passeggeri)
-4. applicare due tipologie di sconto:
-4a. 20% SE minori
-4b. 40% SE maggiori
-5. Prezzo finale
-/*
-  
+
+// 1. Chiedere quanti km si vogliono percorrere
+// 2. Chiedere l'eta del passeggero
+// 3. Calcolare il prezzo totale del viaggio (che si baserà sui km dei passeggeri)
+// 4. applicare due tipologie di sconto:
+// 4a. 20% SE minori
+// 4b. 40% SE maggiori
+// 5. Prezzo finale 
+
+const priceElement = document.getElementById('price')
+// ! console.log('priceElement')
+
+
+// 1. Chiedere quanti km si vogliono percorrere 2. Chiedere l'eta del passeggero
+const userDistance = parseInt(prompt('Quanti km devi effettuare?', 35).trim());
+const userAge = parseInt(prompt('Quanti anni hai?', 30).trim());
+// ! console.log(userDistance, userAge)
+
+
